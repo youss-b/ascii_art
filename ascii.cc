ffd92a85a73d0db985e3533dfd282fb7fb7ec861
Youssef <youssef.biaz@gmail.com>
2021-06-27 19:47:06 -0700

    Add different brightness mappings

--- ascii.cc
+++ ascii.cc
@@ -10,6 +10,16 @@ using namespace Magick;
 
 const string kAsciiMap = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
 
+enum BRIGHT_MAP_TYPE { AVERAGE, MIN_MAX, LUMINOSITY};
+
+struct Flags {
+  bool debug_enabled = false;
+  bool invert = false;
+  string img_file = "";
+  BRIGHT_MAP_TYPE map_type = BRIGHT_MAP_TYPE::AVERAGE;
+  int unsquash_factor = 3;  // TODO: add a way to change this
+};
+
 void Debug(const Color& color) {
   cout << "(" << color.quantumRed() << ", " << color.quantumGreen() << ", " << color.quantumBlue() << ", " << color.quantumAlpha() << ")\n";
 }
@@ -37,9 +47,32 @@ int Average(float a, float b, float c) {
   return 255 * (a + b + c) / 3;
 }
 
-int MapToBrightness(const Image& img, int row, int col) {
+int MinMax(float a, float b, float c) {
+  return 255 * (max(max(a, b), c) + min(min(a, b), c)) / 2;
+}
+
+int Luminosity(double a, double b, double c) {
+  return 255 * (0.21 * a + 0.72 * b + 0.07 * c);
+}
+
+int MapToBrightness(const Image& img, int row, int col, const Flags& flags) {
   ColorRGB  color (img.pixelColor(row, col));
-  return Average(color.red(), color.green(), color.blue());
+  int avg = 0;
+  switch (flags.map_type) {
+    case BRIGHT_MAP_TYPE::MIN_MAX:
+      avg = MinMax(color.red(), color.green(), color.blue());
+      break;
+    case BRIGHT_MAP_TYPE::LUMINOSITY:
+      avg = Luminosity(color.red(), color.green(), color.blue());
+      break;
+    default:
+      avg = Average(color.red(), color.green(), color.blue());
+      break;
+  }
+  if (flags.invert) {
+    avg = 128 + (128 - avg) - 1;
+  }
+  return avg;
 }
 
 char MapToChar(int brightness) {
@@ -48,13 +81,28 @@ char MapToChar(int brightness) {
 }
 
 int main(int argc, char** argv) {
+  Flags flags;
   if (argc < 2) {
     cout << "Not enough arguments! At least need an image filepath...\n";
     return -1;
   }
-  Image image(argv[1]);
+  flags.img_file = argv[1];
+  for (int i = 2; i < argc; ++i) {
+    string str = argv[i];
+    if (string(argv[i]) == "debug") {
+      flags.debug_enabled = true;
+    } else if (string(argv[i]) == "invert") {
+      flags.invert = true;
+    } else if (str == "min_max") {
+      flags.map_type = BRIGHT_MAP_TYPE::MIN_MAX;
+    } else if (str == "lum") {
+      flags.map_type = BRIGHT_MAP_TYPE::LUMINOSITY;
+    }
+  }
+
+  Image image(flags.img_file);
   image.scale("10%x10%");
-  bool debug_enabled = false;
+  bool debug_enabled = flags.debug_enabled;
   if (argc >= 3) {
     string debug_str = argv[2];
     debug_enabled = debug_str == "debug";
@@ -70,8 +118,8 @@ int main(int argc, char** argv) {
   vector<vector<char>> chars(height, vector<char>(width));
   for (int row = 0; row < height; ++row) {
     for (int col = 0; col < width; ++col) {
-      chars[row][col] = MapToChar(MapToBrightness(image, col, row));
-      cout << chars[row][col] << chars[row][col] << chars[row][col];
+      chars[row][col] = MapToChar(MapToBrightness(image, col, row, flags));
+      for (int i = 0; i < flags.unsquash_factor; ++i) cout << chars[row][col];
     }
     cout << endl;
   }
